6284147835f7f634d7e2287ae1b03e706e643ad3
Aarya <amathrej@asu.edu>
2022-06-27 01:11:58 -0700

    Add #multiply to Calculator

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -4,9 +4,15 @@ class Calculator
   def add(*args)
     result = 0
 
-    return 0 if args.length.zero?
-
     args.each { |number| result += number }
+
+    result
+  end
+
+  def multiply(*args)
+    result = 1
+
+    args.each { |number| result *= number }
 
     result
   end
